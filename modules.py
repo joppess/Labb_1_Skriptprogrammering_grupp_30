4f58bbe5a2541507da20fefc52d69e10efaab02e
AndreasL85 <h24andrl@du.se>
2025-01-28 13:20:11 +0100

    Comments

--- modules.py
+++ modules.py
@@ -1,25 +1,29 @@
-from random import randint
+from random import randint # importerar randint från Random-module
 
-def show_divided_numbers(num1, num2):
-    results = []
-    for i in range(1,1401):
-        if i % num1 == 0 and i % num2 == 0:
-            results.append(i)
 
-    return results
+def show_divided_numbers(num1, num2): # vi definierar funktionen med två parametrar
+    results = [] # lista
+    for i in range(1,1401): # for-loop för varje siffra mellan 1 och 1400
+        if i % num1 == 0 and i % num2 == 0: # (if-sats) om valda siffran är delbar med num1 och num2 utan ge någon rest
+            results.append(i) # lägg siffran "i" till listan
 
-def start_game():
-    guessed_number = randint(1,40)
+    return results # vi returnerar listan
 
-    choice = int(input("\ngissa talet mellan 1 och 40: "))
-    while choice != guessed_number:
+def start_game(): #funktion
+    guessed_number = randint(1,40) # slumpa fram ett tal mellan 1 och 40 som sparas i variabel
+
+
+    choice = int(input("\ngissa talet mellan 1 och 40: ")) # vi sparar användarens val i choice genom att konvertera inputen till en int
+    while choice != guessed_number: # while-loop för att loopa så länge gissningen är fel/falsk
         print("Du gissade fel ")
-        if choice < guessed_number:
+        if choice < guessed_number: # kolla om gissningen är mindre än det slumpade talet
             print("Talet är större än din gissning, försök igen ")
-        elif choice > guessed_number:
+        elif choice > guessed_number: # kolla om gissningen är större än det slumpade talet
             print("Talet är mindre än din gissning, försök igen")
 
+        # ge användaren en ny chans att gissa och börja om loopen
         choice = int(input("gissa talet mellan 1 och 40: "))
 
+    # användaren gissade rätt, skriv ut ett meddelande som gratulerar
     print("Du gissade rätt, bra jobbat ")
 
--- program.py
+++ program.py
@@ -1,9 +1,11 @@
-import modules
-first = int(input("Skriv in första talet: "))
+import modules # vi importerar metoderna från modules.py
+first = int(input("Skriv in första talet: ")) # vi frågar användaren och sparar svaret i en variabel
 second = int(input("Skriv in det andra talet: "))
 
-print(f"Resultat: {modules.show_divided_numbers(first, second)}")
+print(f"Resultat: {modules.show_divided_numbers(first, second)}") # vi skriver ut resultatet från metoden show_divided_numbers
 
-modules.start_game()
+modules.start_game() # vi startar spelet genom att anropa start_game
 
-print ("Programmet avslutas ")
+print ("Programmet avslutas ") # programmet avslutas
+
+print("Hello")
